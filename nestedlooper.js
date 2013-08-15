55308516bd5acb4229d3da1cd21e32c646d79a2d
mvegh1 <mvegh1@pride.hofstra.edu>
2013-08-15 14:09:14 -0400

    Update nestedlooper.js

--- nestedlooper.js
+++ nestedlooper.js
@@ -13,3 +13,40 @@ var Looper = function(lb,ub,nestlevel,currentlevel, work, loopVals){
 var LooperWrapper = function(lb,ub,nestlevel,work){
     Looper(lb,ub,nestlevel,0,work,[]);
 }
+
+
+/*** Loop through jagged arrays, now with the option to pass along data and perform callbacks before reaching the deepest nesting ****/
+
+
+
+var Looper2 = function (arr, currentLevel, loopVals, midWork, endWork, data) {
+
+    for (var i = 0; i < arr[currentLevel].length; i++) {
+        loopVals[currentLevel] = i;
+        if (currentLevel != arr.length - 1) {
+            midWork(loopVals, data);
+            Looper2(arr, currentLevel + 1, loopVals, midWork, endWork, data);
+        } else {
+            endWork(loopVals, data);
+        }
+    }
+
+}
+
+var ArrayLoop = function(arr,midWork,endWork,data) {
+   
+   Looper2(arr,0,[],midWork,endWork,data);
+}
+
+ArrayLoop(x, 
+    function (i, d) {
+        d.iteration++;
+        console.log(d)
+    }, 
+    function (i, d) {
+        console.log(i)
+    }, 
+    {
+        "iteration": 0
+    }
+   );
